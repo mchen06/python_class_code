7621667131ff583afcedf91f0c31fe6d6773bba6
Miley Chen <miley.chen.06@gmail.com>
2021-03-24 16:05:38 -0400

    more effecient bubble sort

--- python_projects/bubble_sort.py
+++ python_projects/bubble_sort.py
@@ -4,16 +4,18 @@ def bubble_sort(list):
     # sorts in place
     length_list = len(list) - 1
     comparisons = 0
+    x = 0
     has_swaped = True
     while has_swaped != False:
         has_swaped = False
         y = 0
-        while y < length_list:
+        while y < length_list - x:
             comparisons = comparisons + 1
             if list[y] > list[y + 1]:
               swap_position(list, y, y + 1)
               has_swaped = True
             y = y + 1
+        x = x + 1
     print(f"bubble sort has made {comparisons} comparisons.\n")
     return list
 
